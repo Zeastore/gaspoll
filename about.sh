8ac6ec58d8228e6bbd8a7d328120529353cbe4b6
Zeastore <98459024+Zeastore@users.noreply.github.com>
2022-01-26 18:05:13 +0700

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#     Premium Auto Script By ZEAKING            #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Allah SWT                                     #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Created By : ZEAKING STORE                    #"
+echo -e "# Contact : t.me/zeaking5                       #"
+echo -e "# Whatsapp : NOT FOUND                          #"
+echo -e "================================================="

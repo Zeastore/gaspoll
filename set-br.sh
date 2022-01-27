3a48ccbb55f3cfcbdc88a8a386fa2666b5b3a1d5
Zeastore <98459024+Zeastore@users.noreply.github.com>
2022-01-27 12:05:26 +0700

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/fsidvpn/sc/main/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/Zeastore/gaspoll/main/rclone.conf"
 git clone  https://github.com/magnific0/wondershaper.git
 cd wondershaper
 make install

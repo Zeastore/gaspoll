f058dbcdcb22d9e46e9149ea4062b0d5851c2f3c
Zeastore <98459024+Zeastore@users.noreply.github.com>
2022-02-15 21:03:07 +0700

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -58,7 +58,7 @@ cat> /etc/v2ray/config.json << END
           ]
         },
         "wsSettings": {
-          "path": "/fsid",
+          "path": "/zea",
           "headers": {
             "Host": ""
           }
@@ -146,7 +146,7 @@ cat> /etc/v2ray/none.json << END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/fsid",
+          "path": "/zea",
           "headers": {
             "Host": ""
           }
@@ -243,7 +243,7 @@ cat> /etc/v2ray/vless.json << END
           ]
         },
         "wsSettings": {
-          "path": "/fsid",
+          "path": "/zea",
           "headers": {
             "Host": ""
           }
@@ -330,7 +330,7 @@ cat> /etc/v2ray/vnone.json << END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/fsid",
+          "path": "/zea",
           "headers": {
             "Host": ""
           }

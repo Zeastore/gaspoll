418b7ee4627d625704e276db4e91fe2980b2a88a
Zeastore <98459024+Zeastore@users.noreply.github.com>
2022-04-14 12:13:27 +0700

    Update port-ssl.sh

--- port-ssl.sh
+++ port-ssl.sh
@@ -3,6 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 clear
+stunnelport="$(cat ~/log-install.txt | grep -w "PORT STUNNEL" | cut -d: -f2|sed 's/ //g')"
 ssl="$(cat /etc/stunnel/stunnel.conf | grep -i accept | head -n 2 | cut -d= -f2 | sed 's/ //g' | tr '\n' ' ' | awk '{print $1}')"
 ssl2="$(cat /etc/stunnel/stunnel.conf | grep -i accept | head -n 2 | cut -d= -f2 | sed 's/ //g' | tr '\n' ' ' | awk '{print $2}')"
 echo -e "======================================"

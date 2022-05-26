c0c09f77037e9b40cf815922f449ea5c5c7f56cf
Zeastore <98459024+Zeastore@users.noreply.github.com>
2022-05-27 00:29:37 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,93 @@
 
+# download script
+cd /usr/bin
+wget -O add-host "https://raw.githubusercontent.com/Zeastore/gaspoll/main/add-host.sh"
+wget -O about "https://raw.githubusercontent.com/Zeastore/gaspoll/main/about.sh"
+wget -O menu "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/Zeastore/gaspoll/main/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/Zeastore/gaspoll/main/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/Zeastore/gaspoll/main/hapus.sh"
+wget -O member "https://raw.githubusercontent.com/Zeastore/gaspoll/main/member.sh"
+wget -O delete "https://raw.githubusercontent.com/Zeastore/gaspoll/main/delete.sh"
+wget -O cek "https://raw.githubusercontent.com/Zeastore/gaspoll/main/cek.sh"
+wget -O restart "https://raw.githubusercontent.com/Zeastore/gaspoll/main/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/Zeastore/gaspoll/main/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/Zeastore/gaspoll/main/info.sh"
+wget -O ram "https://raw.githubusercontent.com/Zeastore/gaspoll/main/ram.sh"
+wget -O renew "https://raw.githubusercontent.com/Zeastore/gaspoll/main/renew.sh"
+wget -O autokill "https://raw.githubusercontent.com/Zeastore/gaspoll/main/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/Zeastore/gaspoll/main/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/Zeastore/gaspoll/main/tendang.sh"
+wget -O clear-log "https://raw.githubusercontent.com/Zeastore/gaspoll/main/clear-log.sh"
+wget -O change-port "https://raw.githubusercontent.com/Zeastore/gaspoll/main/change.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/Zeastore/gaspoll/main/port-ovpn.sh"
+wget -O port-ssl "https://raw.githubusercontent.com/Zeastore/gaspoll/main/port-ssl.sh"
+wget -O port-wg "https://raw.githubusercontent.com/Zeastore/gaspoll/main/port-wg.sh"
+wget -O port-tr "https://raw.githubusercontent.com/Zeastore/gaspoll/main/port-tr.sh"
+wget -O port-sstp "https://raw.githubusercontent.com/Zeastore/gaspoll/main/port-sstp.sh"
+wget -O port-squid "https://raw.githubusercontent.com/Zeastore/gaspoll/main/port-squid.sh"
+wget -O port-ws "https://raw.githubusercontent.com/Zeastore/gaspoll/main/port-ws.sh"
+wget -O port-vless "https://raw.githubusercontent.com/Zeastore/gaspoll/main/port-vless.sh"
+wget -O wbmn "https://raw.githubusercontent.com/Zeastore/gaspoll/main/webmin.sh"
+wget -O xp "https://raw.githubusercontent.com/Zeastore/gaspoll/main/xp.sh"
+wget -O bannerku "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/bannerku"
+wget -O bbr "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/bbr.sh"
+wget -O menu "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/menu.sh"
+wget -O trojaan "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/trojaan.sh"
+wget -O vleess "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/vleess.sh"
+wget -O wgr "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/wgr.sh"
+wget -O l2tp "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/l2tp.sh"
+wget -O v2raay "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/v2raay.sh"
+wget -O ssh "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/ssh.sh"
+wget -O sstpp "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/sstpp.sh"
+wget -O ssssr "https://raw.githubusercontent.com/Zeastore/gaspoll/main/menu-all/ssssr.sh"
+wget -O backup "https://raw.githubusercontent.com/Zeastore/gaspoll/main/backup.sh"
+wget -O autobackup "https://raw.githubusercontent.com/Zeastore/gaspoll/main/autobackup.sh"
+wget -O rclone "https://raw.githubusercontent.com/Zeastore/gaspoll/main/rclone.conf"
+
+chmod +x add-host
+chmod +x script-info
+chmod +x menu
+chmod +x usernew
+chmod +x trial
+chmod +x hapus
+chmod +x member
+chmod +x delete
+chmod +x cek
+chmod +x restart
+chmod +x speedtest
+chmod +x info
+chmod +x about
+chmod +x autokill
+chmod +x tendang
+chmod +x ceklim
+chmod +x ram
+chmod +x renew
+chmod +x clear-log
+chmod +x change-port
+chmod +x port-ovpn
+chmod +x port-ssl
+chmod +x port-wg
+chmod +x port-sstp
+chmod +x port-tr
+chmod +x port-squid
+chmod +x port-ws
+chmod +x port-vless
+chmod +x wbmn
+chmod +x clear-log
+chmod +x xp
+chmod +x bannerku
+chmod +x bbr
+chmod +x menu
+chmod +x trojaan
+chmod +x vleess
+chmod +x wgr
+chmod +x l2tp
+chmod +x v2raay
+chmod +x ssh
+chmod +x sstpp
+chmod +x ssssr
+
+cd
+certv2ray
+cd

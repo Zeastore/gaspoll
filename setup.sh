d76aa514d20f358e407da8d8dfbb00d0243c80f3
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-06-03 11:55:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io);
+MYIP=$(wget -qO- ipv4.wildyproject.com);
 IZIN=$( curl https://raw.githubusercontent.com/Zeastore/gaspoll/main/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"

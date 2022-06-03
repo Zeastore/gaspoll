ac7fa3e3c468555a3ea0f66f6f4192d4ee018f74
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-06-03 08:37:09 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -133,6 +133,40 @@ apt -y install squid3
 wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/Zeastore/gaspoll/main/squid3.conf"
 sed -i $MYIP2 /etc/squid/squid.conf
 
+# Install SSLH
+apt -y install sslh
+rm -f /etc/default/sslh
+
+# Settings SSLH
+cat > /etc/default/sslh <<-END
+# Default options for sslh initscript
+# sourced by /etc/init.d/sslh
+
+# Disabled by default, to force yourself
+# to read the configuration:
+# - /usr/share/doc/sslh/README.Debian (quick start)
+# - /usr/share/doc/sslh/README, at "Configuration" section
+# - sslh(8) via "man sslh" for more configuration details.
+# Once configuration ready, you *must* set RUN to yes here
+# and try to start sslh (standalone mode only)
+
+RUN=yes
+
+# binary to use: forked (sslh) or single-thread (sslh-select) version
+# systemd users: don't forget to modify /lib/systemd/system/sslh.service
+DAEMON=/usr/sbin/sslh
+
+DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:80 --pidfile /var/run/sslh/sslh.pid -n"
+
+END
+
+# Restart Service SSLH
+service sslh restart
+systemctl restart sslh
+/etc/init.d/sslh restart
+/etc/init.d/sslh status
+/etc/init.d/sslh restart
+
 # setting vnstat
 apt -y install vnstat
 /etc/init.d/vnstat restart
@@ -162,11 +196,11 @@ socket = r:TCP_NODELAY=1
 accept = 445
 connect =127.0.0.1:8880
 [dropbear]
-accept = 443
-connect = 127.0.0.1:143
+accept = 442
+connect = 127.0.0.1:109
 [dropbear]
 accept = 777
-connect = 127.0.0.1:109
+connect = 127.0.0.1:444
 [openvpn]
 accept = 992
 connect = 127.0.0.1:1194

1a856e2971a2d05008a9f23efcab8140fb59a69e
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-06-03 08:23:17 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -4,10 +4,10 @@ green='\e[0;32m'
 NC='\e[0m'
 clear
 apt install jq curl -y
-DOMAIN=zeaking.me
+#DOMAIN=zeaking.me
 DOMAIN=zeaking.my.id
 sub=$(</dev/urandom tr -dc a-z | head -c4)
-SUB_DOMAIN=${sub}.zeaking.me
+#SUB_DOMAIN=${sub}.zeaking.me
 SUB_DOMAIN=${sub}.zeaking.my.id
 CF_ID=mulahkual@gmail.com
 CF_KEY=1351e3252ad80b9a761d34d125545053403b5
